ba3008623608979c7cd4091113db42e9dbabf929
Justin Moore <Hametsu777@gmail.com>
2022-12-12 20:57:14 -0500

    add img slider

---
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./FeaturedProducts.scss";
+
+export default function FeaturedProducts({ type }) {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Long Sleeve Graphic T-Shirt",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Coat",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Skirt",
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Hat",
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit vitae
+          autem corporis assumenda asperiores reiciendis repellat optio, nobis
+          quia velit accusantium adipisci aliquam debitis, soluta iure quo,
+          expedita temporibus ullam?
+        </p>
+      </div>
+      <div className="bottom"></div>
+    </div>
+  );
+}
